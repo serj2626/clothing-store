c2e5820fdff5b2ed510350a79d58c882a3feec89
serj2626 <serj2626@mail.ru>
2025-06-05 12:53:47 +0300

    new data in api

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -29,6 +29,13 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Clothing Store API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
