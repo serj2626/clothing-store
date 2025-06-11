25de342c4a2a662b467be286695b06c21a3a5d1a
serj2626 <serj2626@mail.ru>
2025-06-11 22:34:49 +0300

    added func by get all contacts

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,29 @@
+<script lang="ts" setup>
+import { api } from "./api";
+import type { IContactsResponse } from "./types";
+const { $api } = useNuxtApp();
+
+await useAsyncData<Record<string, IContactsResponse[]>>(
+  "contacts-info",
+  () => $api(api.contacts.list),
+  {
+    transform: (data: IContactsResponse[]) => {
+      const res: Record<string, IContactsResponse[]> = {};
+      for (const key of data) {
+        if (key.type in res) {
+          res[key.type].push(key);
+        } else {
+          res[key.type] = [key];
+        }
+      }
+      return res;
+    },
+    server: true,
+    lazy: false,
+    staleTime: 360,
+  }
+);
+</script>
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator />
