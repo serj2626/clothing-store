f52d83b428b5ac27487f4cddfc788673e23c4489
serj2626 <serj2626@mail.ru>
2025-06-13 14:44:33 +0300

    created form feedback

--- frontend/components/account/AccountFeedback.vue
+++ frontend/components/account/AccountFeedback.vue
@@ -1,60 +1,89 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <form class="account-logout">
-    <p class="account-logout__text">Форма обратной связи</p>
-  </form>
+  <div class="account-form-feedback">
+    <p class="account-form-feedback__title">Форма обратной связи</p>
+
+    <form class="account-form-feedback__form" @submit.prevent="$emit('submit')">
+      <div class="account-form-feedback__form-group">
+        <label for="login-email">Имя</label>
+        <BaseInput
+          :animate="false"
+          radius="8px"
+          type="text"
+          placeholder="Ваше имя "
+        />
+      </div>
+
+      <div class="account-form-feedback__form-group">
+        <label for="login-password">Телефон</label>
+        <BaseInput
+          :animate="false"
+          radius="8px"
+          type="text"
+          placeholder="8-888-888-88-88"
+        />
+      </div>
+      <div class="account-form-feedback__form-group">
+        <textarea
+          class="account-form-feedback__form-group-textarea"
+          placeholder="Ваше сообщение"
+        />
+      </div>
+      <BaseButton
+        class="account-form-feedback__form-submit"
+        label="Отправить"
+        size="lg"
+        radius="8px"
+        color="#e0bea2"
+      />
+    </form>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.account-logout {
-  padding: 30px;
-  background-color: $white;
-  border-radius: 16px;
-  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
-  max-width: 400px;
-  margin: 40px auto;
-  text-align: center;
-
-  &__text {
-    font-size: 18px;
-    font-weight: 500;
+.account-form-feedback {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 600px;
+  width: 100%;
+  margin-inline: auto;
+  &__title {
+    font-size: 24px;
     color: $txt;
-    margin-bottom: 30px;
+    font-weight: 500;
   }
 
-  &__actions {
+  &__form {
     display: flex;
-    justify-content: center;
-    gap: 20px;
+    flex-direction: column;
+    gap: 10px;
+    &-submit {
+      width: 100%;
+      margin-top: 15px;
+    }
+    &-group {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
 
-    &-btn {
-      padding: 10px 24px;
-      border: none;
-      border-radius: 8px;
-      font-size: 16px;
-      font-weight: 500;
-      cursor: pointer;
-      transition: all 0.2s ease-in-out;
-
-      &--yes {
-        background-color: $error;
-        color: #fff;
-
-        &:hover {
-          background-color: #d32f2f;
-        }
-      }
-
-      &--no {
-        background-color: #e0e0e0;
+      label {
+        font-family: $ff_second;
+        font-size: 14px;
         color: $txt;
-
-        &:hover {
-          background-color: #bdbdbd;
-        }
+        font-weight: 500;
+      }
+      &-textarea {
+        resize: vertical;
+        margin-top: 8px;
+        width: 100%;
+        max-height: 200px;
+        height: 100px;
+        border-radius: 8px;
+        border: 1px solid $txt;
+        padding-block: 16px;
+        padding-left: 20px;
       }
     }
   }
